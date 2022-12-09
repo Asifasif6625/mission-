f81230cef4b4896352cc41e3c2eac855c7e5f1a6
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2022-12-10 02:30:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Hackermanker/Autofilter-bot.git /Autofilter-bot
+  git clone https://github.com/Asifasif6625/mission-.git /mission-
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Autofilter-bot
+  git clone $UPSTREAM_REPO /mission-
 fi
-cd /Autofilter-bot
+cd /mission-
 pip3 install -U -r requirements.txt
 echo "Starting Autofilter-bot...."
 python3 bot.py
